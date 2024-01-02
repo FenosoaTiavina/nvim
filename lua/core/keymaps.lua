915ddafd08f561e3cb3a41dac7d5d62151390926
FensoaTiavina <fenosoa.t.r@gmail.com>
2024-01-02 22:25:41 +0300

    Added all necessities maybe<?>

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,34 +1,36 @@
-local function map(mode, lhs, rhs)
-	vim.keymap.set(mode, lhs, rhs, { silent = true })
+local function map(mode, lhs, rhs , opts)
+    opts.silent = true
+    vim.keymap.set(mode, lhs, rhs, opts)
 end
 
 -- <leader> = the space key
 
 -- Save
-map("n", "<leader>w", "<CMD>update<CR>")
+map("n", "<leader>w", "<CMD>update<CR>", {desc ="Update"})
 
 -- Quit
-map("n", "<leader>q", "<CMD>q<CR>")
+map("n", "<leader>q", "<CMD>q<CR>", {desc ="Quit"})
 
 -- Exit insert mode
-map("i", "jk", "<ESC>")
+map("i", "jk", "<ESC>", {desc ="Exit insert mode"})
 
 -- Windows
-map("n", "<leader>sv", "<CMD>vsplit<CR>")
-map("n", "<leader>sh", "<CMD>split<CR>")
-
+map("n", "<leader>sv", "<CMD>vsplit<CR>", {desc ="Vertical split"})
+map("n", "<leader>sh", "<CMD>split<CR>", {desc ="Horizontal split"})
 -- Buffer
-map("n", "<TAB>", "<CMD>bnext<CR>")
-map("n", "<S-TAB>", "<CMD>bprevious<CR>")
+map("n", "<TAB>", "<CMD>bnext<CR>", {desc ="Next Buffer"})
+map("n", "<S-TAB>", "<CMD>bprevious<CR>", {desc ="Previous Buffer"})
+map("n", "<leader>bd", "<CMD>bdelete<CR>", {desc = "Close current buffer"})
+map("n", "<leader>bf", vim.lsp.buf.format, {desc="Format current buffer"})
 
 -- Window Navigation
-map("n", "<C-h>", "<C-w>h")
-map("n", "<C-l>", "<C-w>l")
-map("n", "<C-k>", "<C-w>k")
-map("n", "<C-j>", "<C-w>j")
+map("n", "<C-h>", "<C-w>h", {desc ="Swicth Window up"})
+map("n", "<C-l>", "<C-w>l", {desc ="Swicth Window right"})
+map("n", "<C-k>", "<C-w>k", {desc ="Swicth Window down"})
+map("n", "<C-j>", "<C-w>j", {desc ="Swicth Window left"})
 
 -- Resize Windows
-map("n", "<C-Left>", "<C-w><")
-map("n", "<C-Right>", "<C-w>>")
-map("n", "<C-Up>", "<C-w>+")
-map("n", "<C-Down>", "<C-w>-")
+map("n", "<C-Left>", "<C-w><", {desc ="Resize Window Left"})
+map("n", "<C-Right>", "<C-w>>", {desc ="Resize Window Right"})
+map("n", "<C-Up>", "<C-w>+", {desc ="Resize Window Up"})
+map("n", "<C-Down>", "<C-w>-", {desc ="Resize Window Down"})
